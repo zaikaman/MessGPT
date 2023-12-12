e70cb28a3d4b10444350a373f2e4ec7e9f0f4405
zaikaman <91252891+zaikaman@users.noreply.github.com>
2023-12-12 17:43:52 +0700

    Update MessGPT.py

--- MessGPT.py
+++ MessGPT.py
@@ -26,8 +26,8 @@ try:
     driver.get('http://www.facebook.com')
 
     # Giả sử bạn đã có biến username và password
-    username = 'codedi4756@jalunaki.com'
-    password = 'lovelybaby'
+    username = 'subthinh6@gmail.com'
+    password = 'Lovelybaby93'
 
     # Đợi cho đến khi phần tử 'email' xuất hiện
     wait = WebDriverWait(driver, 10)
@@ -83,6 +83,12 @@ try:
 
                 # Nhấn phím Enter để gửi tin nhắn
                 ActionChains(driver).send_keys(Keys.RETURN).perform()
+                
+                time.sleep(2)
+                
+                # Tìm và click vào nút
+                button = driver.find_element(By.CSS_SELECTOR, 'div.x1i10hfl.x1qjc9v5.xjbqb8w.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.x13fuv20.xu3j5b3.x1q0q8m5.x26u7qi.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xdl72j9.x2lah0s.xeuugli.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1n2onr6.x16tdsg8.x1hl2dhg.xggy1nq.x1ja2u2z.x1t137rt.x1o1ewxj.x3x9cwd.x1q0g3np.x87ps6o.x1lku1pv.x1a2a7pz')
+                button.click()
 
                 # Cập nhật tin nhắn cuối cùng đã gửi
                 last_sent_message = content2
